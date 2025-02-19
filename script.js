e0254794dd27d8dd75b24c2407285e06cb1098fa
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-02-19 14:50:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const makeChange = (c) => {
-  // your name here
+
+	// const obj=[
+	// 	{"p":1},
+	// 	{"n":5},
+	// 	{"d":10},
+	// 	{"q":25},
+	// 		]
+
+
+	const arr = new Array();
+	
+	arr.push({"q":math.floor(c/25)});
+	c%=25;
+	
+	arr.push({"d":math.floor(c/10)});
+	c%=10;
+	arr.push({"n":math.floor(c/5)})
+	c%=5;
+	arr.push({"p":math.floor(c/1)});
+	return arr;
+	
 };
 
 // Do not the change the code below
