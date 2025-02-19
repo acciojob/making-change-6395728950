95cb69b6bc2e90401086d27582b6fca0d0a485f5
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-02-19 14:54:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
 const makeChange = (c) => {
-
-	// const obj=[
-	// 	{"p":1},
-	// 	{"n":5},
-	// 	{"d":10},
-	// 	{"q":25},
-	// 		]
-
-
-	const arr = new Array();
-	
-	arr.push({"q":math.floor(c/25)});
-	c%=25;
-	
-	arr.push({"d":math.floor(c/10)});
-	c%=10;
-	arr.push({"n":math.floor(c/5)})
-	c%=5;
-	arr.push({"p":math.floor(c/1)});
-	return arr;
-	
+    let result = {};
+    
+    result["q"] = Math.floor(c/25);
+    c %= 25;
+    
+    result["d"] = Math.floor(c/10);
+    c %= 10;
+    
+    result["n"] = Math.floor(c/5);
+    c %= 5;
+    
+    result["p"] = Math.floor(c/1);
+    
+    return result;
 };
 
 // Do not the change the code below
